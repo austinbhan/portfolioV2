dece233741465a977cd0e299b6aeff67226a833b
austinbhan <austinbhan@outlook.com>
2023-05-25 15:43:56 -0700

    First commit with headers

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,18 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 
+import styles from './app.css';
+
 export default function App() {
   return(
-    <Router>
-      <h1>Hello World</h1>
-    </Router>
+    <div className={styles.App}>
+      <Router>
+        <h1>Austin Han</h1>
+        <h1>About Me</h1>
+        <h1>Tech Stack</h1>
+        <h1>Projects</h1>
+        <h1>Contact Me</h1>
+      </Router>
+    </div>
   );
 }
